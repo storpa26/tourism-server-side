b50503b852851fc33b7530cf1af96c0180009c4d
storpa26 <tamimfisher02@gmail.com>
2021-11-01 21:31:01 +0600

    baal

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
 const { MongoClient } = require('mongodb');
-
+const objectId = require('mongodb').objectId
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jlnj9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -28,6 +28,14 @@ async function run() {
             const cursor = servicesCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        //get single item
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: objectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
         })
 
         //post api
